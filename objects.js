717bbe487553bf22c129efa5aec32f9b937017bf
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-07 04:22:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 playlist= {[artistName]:songTitle}
 return playlist
 }
-function removeFromPlaylist(playlist, artistName,songTitle){
+function removeFromPlaylist(playlist, artistName){
 delete playlist.artistName
 }
